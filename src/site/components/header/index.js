835e6e825005d2ef17fff2158c7c4effdbdbde32
ooobo <rossrchrdsn@gmail.com>
2020-03-20 15:10:04 +1100

    add datepicker logic, datepicker html/css. add corsanywhere to fetch-scores request

--- src/site/components/header/index.js
+++ src/site/components/header/index.js
@@ -1,15 +1,76 @@
 import { h } from 'preact'
 import style from './style'
 
+
+export function validate(str) {
+    var y = str.substr(0,4),
+        m = str.substr(4,2) - 1,
+        d = str.substr(6,2);
+    var D = new Date(y,m,d);
+    return (D.getFullYear() == y && D.getMonth() == m && D.getDate() == d) ? D : 'invalid date';
+}
+
+export function zeroPad (num) {
+  return num < 10 ? '0' + num : num
+}
+
+export function parseQuery (qstr) {
+  let query = {}
+  let a = qstr.substr(1).split('&')
+  for (let i = 0; i < a.length; i++) {
+    let b = a[i].split('=')
+    query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '')
+  }
+  return query
+}
+
+export function today () {
+  let qdate = new Date();
+  if (window.location.search) {
+    let qs = parseQuery(window.location.search);
+    if (qs.date) {
+      qs.date = validate(qs.date);
+      qdate = qs.date;
+    }
+  } else {qdate = new Date(qdate.setDate(qdate.getDate()))}
+  return `${qdate.getFullYear()}-${zeroPad(qdate.getMonth() + 1)}-${zeroPad(qdate.getDate())}`
+}
+
+export function yday () {
+  let qdate = new Date();
+  if (window.location.search) {
+    let qs = parseQuery(window.location.search);
+    if (qs.date) {
+      qs.date = validate(qs.date);
+      qdate = new Date(qs.date.setDate(qs.date.getDate() - 1));
+    }
+  } else {qdate = new Date(qdate.setDate(qdate.getDate() - 1));}
+  return `?date=${qdate.getFullYear()}${zeroPad(qdate.getMonth() + 1)}${zeroPad(qdate.getDate())}`
+}
+
+export function tmrw () {
+  let qdate = new Date();
+  if (window.location.search) {
+    let qs = parseQuery(window.location.search);
+    if (qs.date) {
+      qs.date = validate(qs.date);
+      qdate = new Date(qs.date.setDate(qs.date.getDate() + 1));
+      
+    }
+  } else {qdate = new Date(qdate.setDate(qdate.getDate() + 1));}
+  return `?date=${qdate.getFullYear()}${zeroPad(qdate.getMonth() + 1)}${zeroPad(qdate.getDate())}`
+}
+
 const Header = () => (
   <div>
     <div class='flex center flex-column header'>
-      <h1 class={style.title}>Worth a watch?</h1>
-      <div class={style.subtitle}>
-        <p class={style.quote}>Then there’s the absolute degenerates, those who fell in love with a city or a team and have to navigate five-hour (or more) time differences to keep up with early games tipping off after midnight their time.</p>
-        <a href='https://theathletic.com/211607/2018/01/20/trusting-the-process-from-across-the-atlantic-ocean/' class={style.attribution}>Derek Bodner, The Athletic</a>
-        {/* There are many different classifications of NBA fans overseas */}
-      </div>
+      <h1 class={style.title}>&nbsp;Worth a watch?&nbsp;</h1>
+      <date class={style.date}>
+      <a href={yday()} class={style.arrow}>&larr;</a>
+      <input type='date' id='date' name='date' class={style.dateinput} value={today()} />
+      <a href={tmrw()} class={style.arrow}>&rarr;</a>
+      
+      </date>
     </div>
     <div class='flex flex-column' />
   </div>
